1ff2dbea7f402f8036f60f8d8e4b97cef8477f65
stgeke <stgeke@gmail.com>
2022-12-01 16:42:26 +0100

    add support for time averaging for user defined quantities + update gabls example

--- src/plugins/tavg.cpp
+++ src/plugins/tavg.cpp
@@ -1,18 +1,3 @@
-/*
-     compute running averages E(X), E(X*X)
-     and E(X*Y) for veloctiy only
-
-     statistics can be obtained by:
-
-     avg(X)   := E(X)
-     var(X)   := E(X*X) - E(X)*E(X)
-     cov(X,Y) := E(X*Y) - E(X)*E(Y)
-
-     Note: The E-operator is linear, in the sense that the expected
-           value is given by E(X) = 1/N * avg[ E(X)_i ], where E(X)_i
-           is the expected value of the sub-ensemble i (i=1...N).
- */
-
 #include "nrs.hpp"
 #include "nekInterfaceAdapter.hpp"
 #include "tavg.hpp"
@@ -29,6 +14,9 @@ static occa::memory o_Urm2;
 static occa::memory o_Pavg, o_Prms;
 static occa::memory o_Savg, o_Srms;
 
+tavg::fieldPairs userFieldList;
+static occa::memory o_userFieldAvg;
+
 static occa::kernel EXKernel;
 static occa::kernel EXXKernel;
 static occa::kernel EXYKernel;
@@ -42,7 +30,29 @@ static dfloat atime;
 static dfloat timel;
 
 static int outfldCounter = 0;
+
 } // namespace
+
+
+static void EX(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_EX)
+{
+  EXKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_EX);
+}
+
+static void EX(dlong N, dlong fieldOffset, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_EX)
+{
+  EXKernel(N, fieldOffset, nflds, a, b, o_x, o_EX);
+}
+
+static void EXX(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_EXX)
+{
+  EXXKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_EXX);
+}
+
+static void EXY(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_y, occa::memory o_EXY)
+{
+  EXYKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_y, o_EXY);
+}
 
 void tavg::buildKernel(occa::properties kernelInfo)
 {
@@ -75,30 +85,16 @@ void tavg::reset()
   atime = 0;
 }
 
-void tavg::EX(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_EX)
-{
-  EXKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_EX);
-}
-
-void tavg::EXX(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_EXX)
-{
-  EXXKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_EXX);
-}
-
-void tavg::EXY(dlong N, dfloat a, dfloat b, int nflds, occa::memory o_x, occa::memory o_y, occa::memory o_EXY)
-{
-  EXYKernel(N, nrs->fieldOffset, nflds, a, b, o_x, o_y, o_EXY);
-}
-
 void tavg::run(dfloat time)
 {
-  if (!nrs->timeStepConverged)
-    return;
-
   if (!setupCalled || !buildKernelCalled) {
-    std::cout << "tavg::run() was called prior to tavg::setup()!\n";
+    if(platform->comm.mpiRank == 0)
+      std::cout << "tavg::run() was called prior to tavg::setup()!\n";
     ABORT(1);
   }
+
+  if (!nrs->timeStepConverged)
+    return;
 
   if (!counter) {
     atime = 0;
@@ -118,66 +114,98 @@ void tavg::run(dfloat time)
   mesh_t *mesh = nrs->meshV;
   const dlong N = mesh->Nelements * mesh->Np;
 
-  // velocity
-  EX(N, a, b, nrs->NVfields, nrs->o_U, o_Uavg);
-  EXX(N, a, b, nrs->NVfields, nrs->o_U, o_Urms);
-
-  const dlong offsetByte = nrs->fieldOffset * sizeof(dfloat);
-  occa::memory o_vx = nrs->o_U + 0 * offsetByte;
-  occa::memory o_vy = nrs->o_U + 1 * offsetByte;
-  occa::memory o_vz = nrs->o_U + 2 * offsetByte;
-
-  EXY(N, a, b, 1, o_vx, o_vy, o_Urm2 + 0 * offsetByte);
-  EXY(N, a, b, 1, o_vy, o_vz, o_Urm2 + 1 * offsetByte);
-  EXY(N, a, b, 1, o_vz, o_vx, o_Urm2 + 2 * offsetByte);
-
-  // pressure
-  EX(N, a, b, 1, nrs->o_P, o_Pavg);
-  EXX(N, a, b, 1, nrs->o_P, o_Prms);
-
-  // scalars
-  if (nrs->Nscalar) {
-    cds_t *cds = nrs->cds;
-    const dlong N = cds->mesh[0]->Nelements * cds->mesh[0]->Np;
-    EX(N, a, b, cds->NSfields, cds->o_S, o_Savg);
-    EXX(N, a, b, cds->NSfields, cds->o_S, o_Srms);
+  if(userFieldList.size()) {
+    int cnt = 0;
+    for(auto& entry : userFieldList) {
+      auto& o_x = std::get<0>(entry);
+      auto& o_y = std::get<1>(entry);
+      auto o_avg = o_userFieldAvg.slice(cnt*nrs->fieldOffset*sizeof(dfloat), nrs->fieldOffset*sizeof(dfloat));
+      if(o_x.ptr() && o_y.ptr() == nullptr) {
+        EX(N, a, b, 1, o_x, o_avg);
+      } else {
+        if(o_x.ptr() == o_y.ptr())
+          EXX(N, a, b, 1, o_x, o_avg);
+        else
+          EXY(N, a, b, 1, o_x, o_y, o_avg);
+      }
+      cnt++;
+    }
+  } else {
+    // velocity
+    EX(N, a, b, nrs->NVfields, nrs->o_U, o_Uavg);
+    EXX(N, a, b, nrs->NVfields, nrs->o_U, o_Urms);
+ 
+    const dlong offsetByte = nrs->fieldOffset * sizeof(dfloat);
+    occa::memory o_vx = nrs->o_U + 0 * offsetByte;
+    occa::memory o_vy = nrs->o_U + 1 * offsetByte;
+    occa::memory o_vz = nrs->o_U + 2 * offsetByte;
+ 
+    EXY(N, a, b, 1, o_vx, o_vy, o_Urm2 + 0 * offsetByte);
+    EXY(N, a, b, 1, o_vy, o_vz, o_Urm2 + 1 * offsetByte);
+    EXY(N, a, b, 1, o_vz, o_vx, o_Urm2 + 2 * offsetByte);
+ 
+    // pressure
+    EX(N, a, b, 1, nrs->o_P, o_Pavg);
+    EXX(N, a, b, 1, nrs->o_P, o_Prms);
+ 
+    // scalars
+    if (nrs->Nscalar) {
+      cds_t *cds = nrs->cds;
+      const dlong N = cds->mesh[0]->Nelements * cds->mesh[0]->Np;
+      EX(N, a, b, cds->NSfields, cds->o_S, o_Savg);
+      EXX(N, a, b, cds->NSfields, cds->o_S, o_Srms);
+    }
   }
 
   timel = time;
 }
 
+void tavg::setup(nrs_t *nrs_, const fieldPairs& fields) 
+{
+  userFieldList = fields;
+  setup(nrs_);
+}
+
 void tavg::setup(nrs_t *nrs_)
 {
+  if (setupCalled) {
+    if(platform->comm.mpiRank == 0)
+      std::cout << "invalid second call to tavg::setup()!\n";
+    ABORT(1);
+  }
+
   if (!buildKernelCalled) {
-    std::cout << "tavg::setup() was called prior tavg::buildKernel()!\n";
+    if(platform->comm.mpiRank == 0)
+      std::cout << "tavg::setup() was called prior tavg::buildKernel()!\n";
     ABORT(1);
   }
 
   nrs = nrs_;
   mesh_t *mesh = nrs->meshV;
 
-  if (setupCalled)
-    return;
-
-  o_Uavg = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
-  o_Urms = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
-  platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Uavg);
-  platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Urms);
-
-  o_Urm2 = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
-  platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Urm2);
-
-  o_Pavg = platform->device.malloc(nrs->fieldOffset, sizeof(dfloat));
-  o_Prms = platform->device.malloc(nrs->fieldOffset, sizeof(dfloat));
-  platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Pavg);
-  platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Prms);
-
-  if (nrs->Nscalar) {
-    cds_t *cds = nrs->cds;
-    o_Savg = platform->device.malloc(cds->fieldOffsetSum, sizeof(dfloat));
-    o_Srms = platform->device.malloc(cds->fieldOffsetSum, sizeof(dfloat));
-    platform->linAlg->fill(cds->fieldOffsetSum, 0.0, o_Savg);
-    platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Srms);
+  if(userFieldList.size() == 0) {
+    o_Uavg = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
+    o_Urms = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
+    platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Uavg);
+    platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Urms);
+ 
+    o_Urm2 = platform->device.malloc(nrs->fieldOffset * nrs->NVfields, sizeof(dfloat));
+    platform->linAlg->fill(nrs->fieldOffset * nrs->NVfields, 0.0, o_Urm2);
+ 
+    o_Pavg = platform->device.malloc(nrs->fieldOffset, sizeof(dfloat));
+    o_Prms = platform->device.malloc(nrs->fieldOffset, sizeof(dfloat));
+    platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Pavg);
+    platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Prms);
+ 
+    if (nrs->Nscalar) {
+      cds_t *cds = nrs->cds;
+      o_Savg = platform->device.malloc(cds->fieldOffsetSum, sizeof(dfloat));
+      o_Srms = platform->device.malloc(cds->fieldOffsetSum, sizeof(dfloat));
+      platform->linAlg->fill(cds->fieldOffsetSum, 0.0, o_Savg);
+      platform->linAlg->fill(nrs->fieldOffset, 0.0, o_Srms);
+    }
+  } else {
+    o_userFieldAvg = platform->device.malloc(userFieldList.size()*nrs->fieldOffset*sizeof(dfloat)); 
   }
 
   setupCalled = 1;
@@ -195,23 +223,34 @@ void tavg::outfld(int _outXYZ, int FP64)
   if (!outfldCounter)
     outXYZ = 1;
 
-  occa::memory o_null;
   occa::memory o_Tavg, o_Trms;
 
-  const int Nscalar = nrs->Nscalar;
-  if (nrs->Nscalar) {
-    o_Tavg = o_Savg;
-    o_Trms = o_Srms;
+  if(userFieldList.size()) {
+    writeFld("ust", atime, outfldCounter, outXYZ, FP64, &o_NULL, &o_NULL, &o_userFieldAvg, userFieldList.size());
+  } else {
+    const int Nscalar = nrs->Nscalar;
+    if (nrs->Nscalar) {
+      o_Tavg = o_Savg;
+      o_Trms = o_Srms;
+    }
+ 
+    writeFld("avg", atime, outfldCounter, outXYZ, FP64, &o_Uavg, &o_Pavg, &o_Tavg, Nscalar);
+ 
+    writeFld("rms", atime, outfldCounter, outXYZ, FP64, &o_Urms, &o_Prms, &o_Trms, Nscalar);
+ 
+    writeFld("rm2", atime, outfldCounter, outXYZ, FP64, &o_Urm2, &o_NULL, &o_NULL, 0);
   }
-
-  writeFld("avg", atime, outfldCounter, outXYZ, FP64, &o_Uavg, &o_Pavg, &o_Tavg, Nscalar);
-
-  writeFld("rms", atime, outfldCounter, outXYZ, FP64, &o_Urms, &o_Prms, &o_Trms, Nscalar);
-
-  writeFld("rm2", atime, outfldCounter, outXYZ, FP64, &o_Urm2, &o_null, &o_null, 0);
 
   atime = 0;
   outfldCounter++;
 }
 
-void tavg::outfld() { tavg::outfld(/* outXYZ */ 0, /* FP64 */ 1); }
+void tavg::outfld()
+{ 
+  tavg::outfld(/* outXYZ */ 0, /* FP64 */ 1); 
+}
+
+occa::memory tavg::userFieldAvg()
+{
+  return o_userFieldAvg;
+}

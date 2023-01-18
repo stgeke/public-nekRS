f59e090cc5c6427f15c3bc92227b2824b87eb9fa
Malachi <malachi2@illinois.edu>
2023-01-18 13:13:15 -0700

    use mempool for fusedPlanarAvg  (#913)

--- src/core/platform.cpp
+++ src/core/platform.cpp
@@ -125,6 +125,9 @@ platform_t::platform_t(setupAide &_options, MPI_Comm _commg, MPI_Comm _comm)
 }
 void memPool_t::allocate(const dlong offset, const dlong fields)
 {
+  if (ptr)
+    free(ptr);
+
   ptr = (dfloat *)calloc(offset * fields, sizeof(dfloat));
   if (fields > 0)
     slice0 = ptr + 0 * offset;
@@ -155,6 +158,9 @@ void memPool_t::allocate(const dlong offset, const dlong fields)
 }
 void deviceMemPool_t::allocate(memPool_t &hostMemory, const dlong offset, const dlong fields)
 {
+  if (o_ptr.size())
+    o_ptr.free();
+
   bytesAllocated = (fields * sizeof(dfloat)) * offset;
   o_ptr = platform->device.malloc(bytesAllocated, hostMemory.slice0);
   if (fields > 0)

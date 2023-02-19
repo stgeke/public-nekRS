8930dd81912d51139321c606f8801303eeb5accc
Nadish Saini <nsaini@anl.gov>
2023-02-18 23:00:01 -0600

    working drag calc

--- src/postProcessing/dragCalc.cpp
+++ src/postProcessing/dragCalc.cpp
@@ -8,7 +8,7 @@
 
 // private members
 namespace {
-  static dfloat dragIntegral;
+  static dfloat *dragIntegral;
 
   static int nbID;
   static int NfpTotal;
@@ -25,20 +25,11 @@ namespace {
   static bool setup = 0;
 }
 
-dfloat postProcessing::getDrag()
-{
-  return dragIntegral;
-}
-
-void postProcessing::dragCalc(nrs_t *nrs, std::vector<int> bID, bool verbose)
+dfloat* postProcessing::dragCalc(nrs_t *nrs, std::vector<int> bID)
 {
   mesh_t *mesh = nrs->meshV;
 
   if(!setup){
-    int bIDmin = *min_element(std::begin(bID), std::end(bID));
-    nrsCheck(bIDmin < 1, platform->comm.mpiComm, EXIT_FAILURE,
-	     "Boundary ID for drag calc must be >=1","");
-    
     nbID = bID.size();
     o_bID = platform->device.malloc(nbID * sizeof(int), bID.data());
 
@@ -52,6 +43,8 @@ void postProcessing::dragCalc(nrs_t *nrs, std::vector<int> bID, bool verbose)
     o_area = platform->device.malloc(Nblock * nbID * sizeof(dfloat), area);
 
     platform->options.getArgs("VISCOSITY",mueLam);
+
+    dragIntegral = (dfloat *)calloc(nbID, sizeof(dfloat));
   }
  
   platform->linAlg->fillKernel(Nblock * nbID, 0.0, o_drag);
@@ -60,21 +53,21 @@ void postProcessing::dragCalc(nrs_t *nrs, std::vector<int> bID, bool verbose)
   occa::memory o_SijOij = platform->o_mempool.slice2;
   nrs->SijOijKernel(mesh->Nelements,
 		    nrs->fieldOffset,
-		    1,
+		    0,
 		    mesh->o_vgeo,
 		    mesh->o_D,
 		    nrs->o_U,
 		    o_SijOij);
-  oogs::startFinish(o_SijOij, 9, nrs->fieldOffset, ogsDfloat, ogsAdd, nrs->gsh);
+  oogs::startFinish(o_SijOij, 6, nrs->fieldOffset, ogsDfloat, ogsAdd, nrs->gsh);
 
   auto dragKernel = platform->kernels.get("drag");
   
-  dragKernel(mesh->Nelements,
+  dragKernel(Nblock,
+	     mesh->Nelements,
 	     nrs->fieldOffset,
-	     NfpTotal,
 	     nbID,
 	     mueLam,
-	     1,
+	     o_bID,
 	     mesh->o_sgeo,
 	     mesh->o_vmapM,
 	     mesh->o_EToB,
@@ -85,14 +78,16 @@ void postProcessing::dragCalc(nrs_t *nrs, std::vector<int> bID, bool verbose)
 
   o_drag.copyTo(drag);
   o_area.copyTo(area);
-  dfloat sbuf[2] = {0, 0};
-  for (int n = 0; n < Nblock; n++){
-    sbuf[0] += drag[n];
-    sbuf[1] += area[n];
+  for (int ibID = 0; ibID < nbID; ibID++){
+    dfloat sbuf[2] = {0, 0};
+    for (int n = 0; n < Nblock; n++){
+      sbuf[0] += drag[n];
+      sbuf[1] += area[n];
+    }
+    MPI_Allreduce(MPI_IN_PLACE, sbuf, 2, MPI_DFLOAT, MPI_SUM, platform->comm.mpiComm);
+    dragIntegral[ibID] = sbuf[0]/sbuf[1];
   }
-  MPI_Allreduce(MPI_IN_PLACE, sbuf, 2, MPI_DFLOAT, MPI_SUM, platform->comm.mpiComm);
-  
-  dragIntegral = sbuf[0]/sbuf[1];
-      
   setup = 1;
+
+  return dragIntegral;
 }
